725235ce4a4dc9f3e0e3cc5886c539a2658d44a2
LAPTOP-CNI641BD\WildD <KozhevnikovAA@tech.bcs.ru>
2022-10-31 16:01:49 +0700

    fix: массив с начальными карточками вынести в отдельный файл

--- scripts/script.js
+++ scripts/script.js
@@ -27,56 +27,7 @@ let imgInPopup = document.querySelector(".popup__image");
 let postsContainer = document.querySelector(".posts");
 let postTemplate = document.querySelector("#post-template").content;
 
-const initialCards = [
-  {
-    name: "BMW 507, 1956",
-    link: "https://million-wallpapers.ru/wallpapers/1/19/499402977561835/ch-rnyj-kabriolet-bmv-na-asfalte-okolo-polya.jpg",
-  },
-  {
-    name: "Duesenberg model j534",
-    link: "https://jooinn.com/images/red-vintage-car-6.jpg",
-  },
-  {
-    name: "Pontiac Firebird, 1972",
-    link: "https://amazingclassiccars.com/wp-content/uploads/2022/03/1971-Pontiac-Firebird-Formula-455-001-1536.jpg",
-  },
-  {
-    name: "Ferrari 250 gt Tour de France",
-    link: "https://i.pinimg.com/originals/a5/e1/b8/a5e1b8ea5b02d73623b508d2dabcba9f.jpg",
-  },
-  {
-    name: "Ford Shelby AC Cobra, 1962",
-    link: "https://i.pinimg.com/originals/a5/96/ef/a596ef321694752b8245a9cf053fe7fb.jpg",
-  },
-  {
-    name: "Bugatti Type 57sc Atlantic",
-    link: "https://rmsothebys-cache.azureedge.net/2/8/f/0/f/8/28f0f8db0c5b4cc8bf00a6368b91aa496d222e89.jpg",
-  },
-  {
-    name: "Buick Roadmaster tuning",
-    link: "https://get.wallhere.com/photo/car-vehicle-black-cars-Buick-buick-roadmaster-Benoit-Fraylon-1462521.jpg",
-  },
-  {
-    name: "Mercedes Benz 300 SL Gullwing",
-    link: "https://i.imgur.com/quT8rE8.jpg",
-  },
-  {
-    name: "Porsche 911 Singer",
-    link: "https://cimg4.ibsrv.net/gimg/rennlist.com-vbulletin/1000x666/03_singer_911_san_diego_442c7d8d3cb4aae9863fee7abf61e4be865c16f9.jpg",
-  },
-  {
-    name: "Nissan GTR 2000 Skyline Bosozoku",
-    link: "https://66.media.tumblr.com/a7484d0bbf0b2fe1e2dbc95509b063c1/tumblr_p32u7yGYBZ1ubc1ibo1_1280.jpg",
-  },
-  {
-    name: "Ваз 2101",
-    link: "https://i05.fotocdn.net/s121/e01090d8e39b113d/public_pin_l/2762171144.jpg",
-  },
-  {
-    name: "Рокабилли, 1950",
-    link: "https://live.staticflickr.com/2729/5860268474_42a0ecff03_b.jpg",
-  },
-];
+const initialCards = cards;
 
 function savedValueUserPopup() {
   nameInput.value = profileTitle.textContent;
